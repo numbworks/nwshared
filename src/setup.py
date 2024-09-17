769059bd07525d1d467ad9e9b90328057b6623fa
Rubèn <numbworks@gmail.com>
2024-09-17 15:40:44 +0200

    #4 setup.py: reverted to previous version (indipendent by nwshared.py).

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,12 @@
-from nwshared import MODULE_NAME, MODULE_VERSION
 from setuptools import setup
 
 setup(
-    name = MODULE_NAME,
-    version = MODULE_VERSION,
+    name = "nwshared",
+    version = "1.3.0",
     description = "A collection of shared components.",
     author = "numbworks",
     url = "https://github.com/numbworks/nwshared",
-    py_modules = [MODULE_NAME],
+    py_modules = ["nwshared"],
     install_requires = [
         "matplotlib>=3.8.2",
         "numpy>=1.26.3",
